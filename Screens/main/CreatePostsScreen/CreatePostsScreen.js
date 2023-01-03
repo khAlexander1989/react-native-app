a82c89f3dc9d317c36fe15803eeae69d56dea25d
Alexander Khardin <khporter1984@gmail.com>
2023-01-03 16:06:42 +0300

    add camera to app

--- Screens/main/CreatePostsScreen/CreatePostsScreen.js
+++ Screens/main/CreatePostsScreen/CreatePostsScreen.js
@@ -1,10 +1,34 @@
-import { View, Text } from "react-native";
+import { useState } from "react";
+import { View, Text, TouchableOpacity } from "react-native";
+import { Camera } from "expo-camera";
+
+import SnapBtnIcon from "../../../assets/Icons/snapBtnIcon.svg";
 
 import styles from "./styles";
 
 export default function CreatePostsScreen() {
+  const [camera, setCamera] = useState(null);
+
+  async function takePhoto() {
+    try {
+      const photo = await camera.takePictureAsync();
+      console.log(photo.uri);
+    } catch (err) {
+      console.log(err.message);
+    }
+  }
+
   return (
     <View style={styles.container}>
+      <Camera style={styles.camera} ref={setCamera}>
+        <TouchableOpacity
+          style={styles.snapBtn}
+          onPress={takePhoto}
+          activeOpacity={0.3}
+        >
+          <SnapBtnIcon width={24} height={24} fill="white" />
+        </TouchableOpacity>
+      </Camera>
       <Text>CreatePostsScreen</Text>
     </View>
   );
--- Screens/main/CreatePostsScreen/styles.js
+++ Screens/main/CreatePostsScreen/styles.js
@@ -3,8 +3,25 @@ import { StyleSheet } from "react-native";
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+
+    justifyContent: "flex-start",
+  },
+  camera: {
+    height: 240,
+    marginTop: 32,
+    marginHorizontal: 16,
     justifyContent: "center",
     alignItems: "center",
+
+    borderRadius: 8,
+  },
+  snapBtn: {
+    justifyContent: "center",
+    alignItems: "center",
+    height: 60,
+    width: 60,
+    borderRadius: 30,
+    backgroundColor: "rgba(255, 255, 255, 0.3)",
   },
 });
 
